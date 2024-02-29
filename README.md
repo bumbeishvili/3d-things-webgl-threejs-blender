53d8970c01bca847c638c674f67d669450ba0af7
bumbeishvili <me@davidb.dev>
2024-02-29 14:56:28 +0400

    Initial project webgi

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 ## Project links
 
-• [Three Andy Boilerplate](https://bumbeishvili.github.io/3d-things-webgl-threejs-blender/three-andy-boilerplate/dist/)
+* [Three Andy Boilerplate](https://bumbeishvili.github.io/3d-things-webgl-threejs-blender/three-andy-boilerplate/dist/)
+* [WebGI - Drill](https://bumbeishvili.github.io/3d-things-webgl-threejs-blender/webgi-vanilla-starter/dist/)
---
+++ webgi-vanilla-starter/vite.config.js
@@ -0,0 +1,17 @@
+import {defineConfig, normalizePath} from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
+
+export default defineConfig({
+    base: '/webgi-vanilla-starter/dist',
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: normalizePath(path.resolve(__dirname, './assets') + '/[!.]*'), // 1️⃣
+                    dest: normalizePath('./assets'), // 2️⃣
+                },
+            ],
+        }),
+    ]
+})
